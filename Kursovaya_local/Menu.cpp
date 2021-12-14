5ed47fb476c7400b4b8761bd6d7bddbe88d67c3f
Egor Golikov <20020110ll@gmail.com>
2021-12-14 20:18:22 +0700

    Добавлен класс "Menu"

---
+++ Kursovaya_local/Menu.cpp
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Menu.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+/*Menu::Menu()
+{ }
+Menu::Menu(const char* menu_text, int item_num)
+{
+	strcpy(this->menu_text, menu_text);
+	this->menu_text[511] = '\0';
+
+	this->item_num = item_num;
+}
+
+Menu::~Menu()
+{
+}
+
+void Menu::print()
+{
+	printf("%s", menu_text);
+}
+*/
+
+void Menu::start_menu()
+{
+	system("cls");
+	cout << "Для просмотра списка фильмов нажмите '1'.\nДля входа в режим администратора нажмите '2'.\nДля выхода нажмите 'esc'.\n\n";
+	item_num = 2;
+}
+
+void Menu::user_kassir()
+{
+	cout << "name_kinofeatre";
+	cout << "1) Просмотр списка фильмов\n2) Вход в режим администратора\n\nESC - Выход";
+	item_num = 2;
+}
+
+
+void Menu::input_number()
+{
+	do
+	{
+		menu_number = -1;
+
+		char symbol;
+		symbol = _getch();
+
+		if (symbol == 27)
+		{
+			menu_number = 0;
+		}
+		else if (symbol >= '1' && symbol <= '9')
+		{
+			menu_number = symbol - 48;
+		}
+
+	} while (menu_number < 0 || menu_number > item_num);
+}
+
+void Menu::spisok_kino(Kinoteatr kinoteatr)
+{
+	cout << "Список фильмов:\n\n";
+	for (int i = 0; i < kinoteatr.kol_vo_filmov; i++)
+	{
+		cout << i + 1 << ") " << kinoteatr.filmi[i].name << " (" << kinoteatr.filmi[i].age << ")\n";
+	}
+	_getch();
+}
+
+void Menu::file_choice(int k)
+{
+	cout << "1) Открыть файл по умолчанию в домашнем каталоге (\"kino_v_teatre\")\n2) Открыть файл по заданному пути";
+	if (k == 1)
+	{
+		cout << "\n3)Создание нового файла\n\nESC - выход";
+		item_num = 3;
+	}
+	else
+	{
+		cout << "\n\nESC - выход";
+		item_num = 2;
+	}
+
+}
+
+
---
+++ Kursovaya_local/Menu.h
@@ -0,0 +1,25 @@
+#pragma once
+class Menu
+{
+public:
+	int item_num;
+	int menu_number;
+	//char menu_text[512];
+
+public:
+	//Menu();
+	//Menu(const char* menu_text, int item_num);
+	//~Menu();
+	//void print();
+
+	void user_kassir();
+	void start_menu();
+	void input_number();
+	void file_choice(int k);
+	void spisok_kino(Kinoteatr kinoteatr);
+};
+
+class Menu
+{
+};
+
